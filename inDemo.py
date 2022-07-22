12b476c855e0a2f88c96bf51709d975210917884
Winry <winry.tian@gmail.com>
2022-07-22 11:30:15 -0700

    add implementation of one definition of in to assert understanding, test with different items

--- inDemo.py
+++ inDemo.py
@@ -2,8 +2,19 @@
 # if the item is inside the list. Apparently this works for strings, numbers,
 # and lists, so you can detect nested lists. But you have to get the contents
 # right.
-my_list = ['a', 'b', 'c', [1], 1, list(zip([1], [1]))]
-
-item = list(zip([1], [1]))
-
+my_list = ['a', 'b', 'c', [1], 1, zip([1], [1])]
+item = zip([1], [1])
 print(item in my_list)
+
+
+# here I will also implement the "in" operator using a function
+# ...hmm I have to use to iterate through my_list. this might be a different
+# definition so I'll use it for now
+def my_in(search_item, list_to_search):
+    for item_to_compare in list_to_search:
+        if search_item == item_to_compare:
+            return True
+    return False
+
+
+print(my_in(item, my_list))
